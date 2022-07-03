68711d7b21577bd100ba5ec5abf67341917adb8c
gijoongjang <rlwnd0000@naver.com>
2022-07-03 13:15:01 +0900

    Initial commit

---
+++ src/main/java/com/jpa/spingboot/JpaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/spingboot/JpaSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
