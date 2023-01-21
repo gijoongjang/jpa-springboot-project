87b4326ea1689c914d434d04640285005433aab1
gijoongjang <rlwnd0000@naver.com>
2023-01-21 22:56:14 +0900

    commit

--- src/test/java/com/jpa/spingboot/HelloControllerTest.java
+++ src/test/java/com/jpa/spingboot/HelloControllerTest.java
@@ -1,46 +1,46 @@
-package com.jpa.spingboot;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest
-public class HelloControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void test() throws Exception {
-        String hello = "hello";
-
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
-    }
-
-    @Test
-    public void lombokTest() throws Exception{
-        String name = "장기중";
-        int age = 26;
-
-//        HelloResponseDto hrd = new HelloResponseDto(name, age);
+//package com.jpa.spingboot;
 //
-//        assertThat(hrd.getName()).isEqualTo(name);
-//        assertThat(hrd.getAge()).isEqualTo(age);
-
-        mockMvc.perform(get("/hello/dto")
-                .param("name", name)
-                .param("age", String.valueOf(age)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
-                .andExpect(jsonPath("$.age", is(age)));
-    }
-}
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//import static org.hamcrest.Matchers.is;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest
+//public class HelloControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void test() throws Exception {
+//        String hello = "hello";
+//
+//        mockMvc.perform(get("/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(hello));
+//    }
+//
+//    @Test
+//    public void lombokTest() throws Exception{
+//        String name = "장기중";
+//        int age = 26;
+//
+////        HelloResponseDto hrd = new HelloResponseDto(name, age);
+////
+////        assertThat(hrd.getName()).isEqualTo(name);
+////        assertThat(hrd.getAge()).isEqualTo(age);
+//
+//        mockMvc.perform(get("/hello/dto")
+//                .param("name", name)
+//                .param("age", String.valueOf(age)))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.name", is(name)))
+//                .andExpect(jsonPath("$.age", is(age)));
+//    }
+//}
--- src/test/java/com/jpa/spingboot/domain/post/PostRepositoryTest.java
+++ src/test/java/com/jpa/spingboot/domain/post/PostRepositoryTest.java
@@ -1,68 +1,68 @@
-package com.jpa.spingboot.domain.post;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-public class PostRepositoryTest {
-
-    @Autowired
-    PostRepository postRepository;
-
-    @AfterEach
-    public void cleanup() {
-        postRepository.deleteAll();
-    }
-
-//    @Test
-//    public void findAllPost() {
-//        //given
-//        String title = "test";
-//        String content = "test_content";
+//package com.jpa.spingboot.domain.post;
 //
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//public class PostRepositoryTest {
+//
+//    @Autowired
+//    PostRepository postRepository;
+//
+//    @AfterEach
+//    public void cleanup() {
+//        postRepository.deleteAll();
+//    }
+//
+////    @Test
+////    public void findAllPost() {
+////        //given
+////        String title = "test";
+////        String content = "test_content";
+////
+////        postRepository.save(Posts.builder()
+////                .title(title)
+////                .content(content)
+////                .author("gijoongjang")
+////                .build());
+////
+////        //when
+////        List<Posts> postList = postRepository.findAll();
+////
+////        //then
+////        Posts post = postList.get(0);
+////        assertThat(post.getTitle()).isEqualTo(title);
+////        assertThat(post.getContent()).isEqualTo(content);
+////    }
+//    @Test
+//    public void BaseTimeEntityCreate() {
+//        //given
+//        LocalDateTime now = LocalDateTime.of(2022, 7, 9, 4, 52, 0);
 //        postRepository.save(Posts.builder()
-//                .title(title)
-//                .content(content)
+//                .title("test1")
+//                .content("test1")
 //                .author("gijoongjang")
 //                .build());
 //
 //        //when
-//        List<Posts> postList = postRepository.findAll();
+//        List<Posts> allPost = postRepository.findAll();
 //
 //        //then
-//        Posts post = postList.get(0);
-//        assertThat(post.getTitle()).isEqualTo(title);
-//        assertThat(post.getContent()).isEqualTo(content);
+//        Posts post =  allPost.get(0);
+//
+//        System.out.println("createdDate= " + post.getCreatedDate() + ", modifiedDate= " + post.getModifiedDate());
+//
+//        assertThat(post.getCreatedDate()).isAfter(now);
+//        assertThat(post.getModifiedDate().isAfter(now));
 //    }
-    @Test
-    public void BaseTimeEntityCreate() {
-        //given
-        LocalDateTime now = LocalDateTime.of(2022, 7, 9, 4, 52, 0);
-        postRepository.save(Posts.builder()
-                .title("test1")
-                .content("test1")
-                .author("gijoongjang")
-                .build());
-
-        //when
-        List<Posts> allPost = postRepository.findAll();
-
-        //then
-        Posts post =  allPost.get(0);
-
-        System.out.println("createdDate= " + post.getCreatedDate() + ", modifiedDate= " + post.getModifiedDate());
-
-        assertThat(post.getCreatedDate()).isAfter(now);
-        assertThat(post.getModifiedDate().isAfter(now));
-    }
-}
+//}
